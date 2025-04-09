6eac6a652dc495aa58237adb95ba3ec4e0c48a6f
VinayakX <rasgotravinayak@gmail.com>
2025-04-10 01:06:47 +0530

    Initial Commit

---
+++ EduMantra/src/App.jsx
@@ -0,0 +1,15 @@
+
+import MainPage from './pages/mainPage'
+import Hero from './pages/Hero'
+function App() {
+  
+
+  return (
+    <>
+      {/* <MainPage/> */}
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ EduMantra/src/pages/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+      <>
+          <div className=' bg-gradient-to-r from-slate-900 to-slate-950 h-screen w-screen'> </div>
+        
+      </>
+  )
+}
+
+export default Hero;
