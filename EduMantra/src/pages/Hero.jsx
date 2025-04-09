951643c04f70f2d79fe92fffe8586bf2cb59dc42
VinayakX <rasgotravinayak@gmail.com>
2025-04-10 01:11:21 +0530

    initial

--- EduMantra/src/pages/Hero.jsx
+++ EduMantra/src/pages/Hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { FaRegUser } from "react-icons/fa";
 function Hero() {
   return (
       <>
-          <div className=' bg-gradient-to-r from-slate-900 to-slate-950 h-screen w-screen'> </div>
+          <div className=' bg-gradient-to-r from-slate-900 to-slate-950 h-screen w-screen'>
+              <div>
+              <FaRegUser />  
+          </div>
+          
+          </div>
         
       </>
   )
