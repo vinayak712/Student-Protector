96f8d332eacd39594a79fcdba55146fefed73c2e
VinayakX <rasgotravinayak@gmail.com>
2025-04-10 16:07:35 +0530

    adding backend folder

---
+++ Backend/src/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Port Running on ${port}` );
+    
+})
